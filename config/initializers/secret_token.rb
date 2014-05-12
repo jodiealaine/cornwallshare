b1397bcfc2c27bcd26cfc64e4a28a893b1fdb741
Jodie Parker <jodie@mindfulcoder.co.uk>
2014-05-12 17:22:25 +0100

    initial rails project files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Cornwallshare::Application.config.secret_key_base = '0047eb5f4f4518f35ff54fa744b1590748c2dd664a0e159e314515808d8334370eb4e1727bb3a513d633bd98cd62366a1ac5568b94a52207fa4d20a877e2c714'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Cornwallshare::Application.config.session_store :cookie_store, key: '_cornwallshare_session'
